377c996537021758b2638f012c2c1c83bf223958
Ty <tclints@gmail.com>
2020-03-04 12:12:32 -0800

    Added project "Healthy Hearts At Home" files

---
+++ healthhearts/healthyhearts/healthyhearts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  healthyhearts
+//
+//  Created by Tyler on 1/27/18.
+//  Copyright © 2018 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
